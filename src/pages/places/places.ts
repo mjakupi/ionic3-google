f6e3afe37359ce025b21b4f1b6bd1df47b23382c
mjakupi <jakupi@eagle.mk>
2018-09-27 16:22:19 +0200

    First Commit

---
+++ src/pages/places/places.ts
@@ -0,0 +1,76 @@
+import { Component, NgZone } from '@angular/core';
+import { LoadingController } from 'ionic-angular';
+
+@Component({
+  selector: 'places-map',
+  templateUrl: 'places.html'
+})
+export class PlacesPage {
+  autocomplete: any;
+  GoogleAutocomplete: any;
+  GooglePlaces: any;
+  geocoder: any
+  autocompleteItems: any;
+  nearbyItems: any = new Array<any>();
+  loading: any;
+
+  constructor(
+    public zone: NgZone,
+    public loadingCtrl: LoadingController
+  ) {
+
+    this.geocoder = new google.maps.Geocoder;
+    let elem = document.createElement("div")
+    this.GooglePlaces = new google.maps.places.PlacesService(elem);
+    this.GoogleAutocomplete = new google.maps.places.AutocompleteService();
+    this.autocomplete = {
+      input: ''
+    };
+    this.autocompleteItems = [];
+    this.loading = this.loadingCtrl.create();
+  }
+
+
+  updateSearchResults(){
+    if (this.autocomplete.input == '') {
+      this.autocompleteItems = [];
+      return;
+    }
+    this.GoogleAutocomplete.getPlacePredictions({ input: this.autocomplete.input },
+      (predictions, status) => {
+        this.autocompleteItems = [];
+        if(predictions){
+          this.zone.run(() => {
+            predictions.forEach((prediction) => {
+              this.autocompleteItems.push(prediction);
+            });
+          });
+        }
+    });
+  }
+
+  selectSearchResult(item){
+    this.loading.present();
+    this.autocompleteItems = [];
+    this.geocoder.geocode({'placeId': item.place_id}, (results, status) => {
+
+      if(status === 'OK' && results[0]){
+        this.autocompleteItems = [];
+        this.GooglePlaces.nearbySearch({
+          location: results[0].geometry.location,
+          radius: '500',
+          types: ['restaurant'], //check other types here https://developers.google.com/places/web-service/supported_types
+          key: 'AIzaSyA8QEN3Ykzg6Mr3GvoPkJz_uADis477gMY'
+        }, (near_places) => {
+          this.zone.run(() => {
+            this.nearbyItems = [];
+            for (var i = 0; i < near_places.length; i++) {
+              this.nearbyItems.push(near_places[i]);
+            }
+            this.loading.dismiss();
+          });
+        })
+      }
+    })
+  }
+}
